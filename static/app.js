beaf9f97c1cc0f6185eb4a6a598aff6854f10820
NaveenBurujula <168223373+NaveenBurujula@users.noreply.github.com>
2024-06-18 18:12:50 +0530

    Add files via upload

---
+++ static/app.js
@@ -0,0 +1,87 @@
+class Chatbox {
+    constructor() {
+        this.args = {
+            openButton: document.querySelector('.chatbox__button'),
+            chatBox: document.querySelector('.chatbox__support'),
+            sendButton: document.querySelector('.send__button')
+        };
+
+        this.state = false;
+        this.messages = [];
+    }
+
+    display() {
+        const { openButton, chatBox, sendButton } = this.args;
+
+        openButton.addEventListener('click', () => this.toggleState(chatBox));
+
+        sendButton.addEventListener('click', () => this.onSendButton(chatBox));
+
+        const node = chatBox.querySelector('input');
+        node.addEventListener("keyup", ({ key }) => {
+            if (key === "Enter") {
+                this.onSendButton(chatBox);
+            }
+        });
+    }
+
+    toggleState(chatBox) {
+        this.state = !this.state;
+
+        // show or hides the box
+        if (this.state) {
+            chatBox.classList.add('chatbox--active');
+        } else {
+            chatBox.classList.remove('chatbox--active');
+        }
+    }
+
+    onSendButton(chatBox) {
+        var textField = chatBox.querySelector('input');
+        let text = textField.value;
+        if (text === "") {
+            return;
+        }
+
+        let msg = { name: "User", message: text };
+        this.messages.push(msg);
+
+        fetch('/predict', {
+            method: 'POST',
+            body: JSON.stringify({ message: text }),
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+        .then(r => r.json())
+        .then(r => {
+            let msg2 = { name: "sam", message: r.answer };
+            this.messages.push(msg2);
+            this.updateChatText(chatBox);
+            textField.value = '';
+        }).catch((error) => {
+            console.error('Error:', error);
+            this.updateChatText(chatBox);
+            textField.value = '';
+        });
+    }
+
+    updateChatText(chatBox) {
+        var html = '';
+        this.messages.slice().reverse().forEach(function(item, index) {
+            if (item.name === "sam") {
+                html += '<div class="messages__item messages__item--visitor">' + item.message + '</div>';
+            } else {
+                html += '<div class="messages__item messages__item--operator">' + item.message + '</div>';
+            }
+        });
+
+        const chatmessage = chatBox.querySelector('.chatbox__messages');
+        chatmessage.innerHTML = html;
+    }
+}
+
+
+const chatbox = new Chatbox();
+chatbox.display();
